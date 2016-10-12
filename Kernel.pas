4efcfb4a4753cc4e5c800e470d79d6ae88aff003
Epik17 <floyd17@yandex.ru>
2016-10-12 14:15:51 +0400

    Kernel.pas visibility tuning

--- Kernel.pas
+++ Kernel.pas
@@ -4,6 +4,7 @@ interface
 
 uses HelicoptersDatabase, Dialogs,Sysutils,World;
 
+
 const
   maxV=360;
 
@@ -11,25 +12,32 @@ type TDiapason = array [0..maxV] of Real;
 
 function HotV(helicopter : THelicopter; V: Real) : Real; overload; //характеризует диапазон высот и скоростей для нормальных условий
 function HotV(helicopter : THelicopter; icG, icT,V: Real) : Real; overload; //характеризует диапазон высот и скоростей с учетом полетного веса и температуры
-function Diapason(helicopter : THelicopter) : TDiapason;overload; //просто сетка для визуализации диапазона для нормальных условий
+
 function Diapason(helicopter : THelicopter; icG, icT : Real) : TDiapason;overload; //просто сетка для визуализации скорректированного диапазона
+
 function nx(helicopter : THelicopter; ny, icG, icT,hManevraCurrent,V : Real) : Real;overload; //с учетом полетного веса и температуры
 function nx(helicopter : THelicopter; ny, icG, icT,hManevraCurrent,hManevraInitial,V : Real) : Real;overload; //с учетом полетного веса, температуры и шага несущего винта
          //вторая функция nx не рекомендуется к использованию; лучше посчитать поправку один раз и потом вычитать ее
 function nxOtXvr(helicopter : THelicopter;hManevraCurrent,icG,V : Real) : Real;
+
 function ny(helicopter : THelicopter;icG, icT,icH0,V : Real):Real;
+
 function VmaxOnAGivenHeight(helicopter : THelicopter; icG, icT, h : Real) : Integer;//на высоте h c точностью до 1 км/ч
 function VminOnAGivenHeight(helicopter : THelicopter; icG, icT, h : Real) : Integer;
+
 function RealHst(helicopter : THelicopter; icG, icT : Real): Real;
 
+
+
 implementation
+
 
 function HotV(helicopter : THelicopter; V: Real) : Real;
 begin
  Result := helicopter.Hdyn - Sqr(V-helicopter.ParabolaCoeff*helicopter.Vmax)*(helicopter.Hdyn-helicopter.Hst)/Sqr(helicopter.ParabolaCoeff*helicopter.Vmax)
 end;
 
-function Diapason (helicopter : THelicopter) : TDiapason;
+function Diapason (helicopter : THelicopter) : TDiapason;overload;  //просто сетка для визуализации диапазона для нормальных условий
 var
   i : Integer;
 begin
